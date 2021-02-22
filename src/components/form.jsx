0d9a4e95e8705c127c95d459b3f7cff32f8f68da
leon kranich <leon_kranich@yahoo.de>
2021-02-22 14:01:49 +0100

    mittag

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -35,8 +35,9 @@ class Form extends Component {
   }
 
   handleSubmit = event => {
-		alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
 		event.preventDefault()
+    const data = this.state;
+    console.log(data);
 	}
 
   render() { 
